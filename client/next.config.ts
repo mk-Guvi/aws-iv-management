b6062dacda75952c101fcfef54af2953c9031b2b
Mohammed Musharaf <mk.guvi123@gmail.com>
2024-12-15 12:01:29 +0530

    Revert "[FEAT] added Sidebar,Navbar,Redux SETUP"
    This reverts commit 5eb205c72b1255b371d26ee12de88d693010bb7a.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['s3-inventorymanagement.s3.us-east-2.amazonaws.com']
-  }
+  /* config options here */
 };
 
 export default nextConfig;
--- client/src/app/DashboardWrapper.tsx
+++ client/src/app/DashboardWrapper.tsx
@@ -1,49 +1,11 @@
-"use client";
+import React from 'react'
 
-import React, { useEffect } from "react";
-import Navbar from "@/app/(components)/Navbar";
-import Sidebar from "@/app/(components)/Sidebar";
-import StoreProvider, { useAppSelector } from "./redux";
+type Props = {children: React.ReactNode}
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  const isSidebarCollapsed = useAppSelector(
-    (state) => state.global.isSidebarCollapsed
-  );
-  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.add("light");
-    }
-  });
-
+function DashboardWrapper({children}: Props) {
   return (
-    <div
-      className={`${
-        isDarkMode ? "dark" : "light"
-      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
-    >
-      <Sidebar />
-      <main
-        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
-          isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
-        }`}
-      >
-        <Navbar />
-        {children}
-      </main>
-    </div>
-  );
-};
+    <div>{children}</div>
+  )
+}
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <StoreProvider>
-      <DashboardLayout>{children}</DashboardLayout>
-    </StoreProvider>
-  );
-};
-
-export default DashboardWrapper;
+export default DashboardWrapper
