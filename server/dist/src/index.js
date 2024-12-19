8cbfa5f78fe447588dd2d611ac5e4062d8ab94bb
Mohammed Musharaf <mk.guvi123@gmail.com>
2024-12-19 07:31:17 +0530

    [FEAT] added inventory page and api routes for inventory

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -10,6 +10,7 @@ const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
+const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
@@ -20,6 +21,7 @@ app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
 app.use("/dashboard", dashboardRoutes_1.default); // http://localhost:8000/dashboard
+app.use("/products", productRoutes_1.default); // http://localhost:8000/products
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port ${port}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import dashboardRoutes from "./routes/dashboardRoutes";
+import productRoutes from "./routes/productRoutes";
 
 
 
@@ -20,6 +21,7 @@ app.use(cors());
 
 
 app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
+app.use("/products", productRoutes); // http://localhost:8000/products
  
 
 
